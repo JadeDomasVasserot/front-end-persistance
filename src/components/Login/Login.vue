c84069fc5e7004750a98a0addb25c3387642bd98
hugobriday <hugo.briday@gmail.com>
2023-03-01 15:20:10 +0100

    page login

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,41 +1,37 @@
-    <template>
-        
-        <div class="d-flex align-center justify-center" style="height: 100vh">
-           
+<template>
+     <div class="d-flex align-center justify-center" style="height: 100vh">
             <v-sheet width="400" class="mx-auto">
+                <v-img class="pb-8" :src="imageSrc" alt="Image" />
+
                 <v-form fast-fail @submit.prevent="login">
-                    <v-text-field  variant="underlined" v-model="username" label="User Name"></v-text-field>
-
-                    <v-text-field variant="underlined" v-model="password" label="password"></v-text-field>
-                    <a href="#" class="text-body-2 font-weight-regular">Forgot Password?</a>
-
-                    <v-btn type="submit" variant="outlined" color="primary" block class="mt-2">Sign in</v-btn>
-
+                    <v-text-field variant="underlined" v-model="username" label="Email"></v-text-field>
+                    <v-text-field variant="underlined" v-model="password" label="Mot de passe"></v-text-field>
+                    <a href="#" class="text-body-2 font-weight-regular">Mot de passe oubliée?</a>
+                    <v-btn type="submit" variant="outlined" color="#CD3C6F" block class="mt-2">Se connecter</v-btn>
                 </v-form>
                 <div class="mt-2">
-                    <p class="text-body-2">Don't have an account? <a href="#">Sign Up</a></p>
+                    <p class="text-body-2">Vous n'avez pas de compte ?  <a href="#">Inscrivez-vous</a></p>
                 </div>
             </v-sheet>
-           
-        </div>
-        <div class="d-flex align-right justify-center">
-            <img :src="imageSrc" alt="Image" />
-        </div>
-                            
-    </template>
-    <script>
-    export default {
-        data() {
-            return {
-                username: '',
-                password: '',
-                imageSrc: 'src/assets/image.png',
-            };
+        </div >
+</template>
+<script>
+export default {
+    data() {
+        return {
+            username: '',
+            password: '',
+            imageSrc: 'src/assets/logo.png',
+        };
+    },
+    methods: {
+        login() {
+            // Your login logic here
+            this.$router.push('/ListArticle')
         },
-        methods: {
-            login() {
-                // Your login logic here
-            },
-        },
-    }
-    </script>
+    },
+}
+</script>
+<style>
+
+</style>
